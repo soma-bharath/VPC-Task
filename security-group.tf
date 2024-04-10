5ea50607aebaf482cae383662fc71b404880dad0
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-04-10 18:54:46 +0530

    Update security-group.tf

--- security-group.tf
+++ security-group.tf
@@ -11,6 +11,20 @@ resource "aws_security_group" "eks_cluster_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "ssh"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # Egress rules for control plane security group
   egress {
     from_port   = 0
