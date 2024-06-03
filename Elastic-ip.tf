e76db93f22fc3f9b72eced6f673e1d610594bb8e
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-06-03 17:55:41 +0530

    Update Elastic-ip.tf

--- Elastic-ip.tf
+++ Elastic-ip.tf
@@ -1,8 +1,10 @@
 resource "aws_eip" "my_eip" {
   vpc = true
   tags = {
-    Name = "My-EIP"
+    Name = var.Name
+    Environment  = var.env
+    Email   = var.Email
+    Project_Name = var.Project_Name
     Date = local.current_date
-    Env  = var.env
   }
 }
