1ca0d26ed388f2520739c601290f5ad05ffa8f3d
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-06-03 17:55:55 +0530

    Update Nat-Gateway.tf

--- Nat-Gateway.tf
+++ Nat-Gateway.tf
@@ -2,8 +2,10 @@ resource "aws_nat_gateway" "my_nat_gateway" {
   allocation_id = aws_eip.my_eip.id
   subnet_id     = aws_subnet.public_subnet_1.id
   tags = {
-    Name = "NATGateway"
+    Name = var.Name
+    Environment  = var.env
+    Email   = var.Email
+    Project_Name = var.Project_Name
     Date = local.current_date
-    Env  = var.env
   }
 }
