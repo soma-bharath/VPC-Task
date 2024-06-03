c48bb9e3aa17c1d7cbeacdf6228ec13918e27ead
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-06-03 18:30:55 +0530

    Update public-route-table.tf

--- public-route-table.tf
+++ public-route-table.tf
@@ -1,15 +1,17 @@
 resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = data.aws_vpc.main_vpc.id
 
   route {
     cidr_block     = "0.0.0.0/0"
     gateway_id     = aws_internet_gateway.my_igw.id
   }
 
-  tags = {
-    Name = "Public Route Table"
+  tags = {    
+    Name = "Public-Route-Table-spaces-prod"
+    Environment  = var.env
+    Email   = var.Email
+    Project_Name = var.Project_Name
     Date = local.current_date
-    Env  = var.env
   }
 }
 
@@ -20,5 +22,10 @@ resource "aws_route_table_association" "public_subnet_association_1" {
 
 resource "aws_route_table_association" "public_subnet_association_2" {
   subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_subnet_association_3" {
+  subnet_id      = aws_subnet.public_subnet_3.id
   route_table_id = aws_route_table.public_route_table.id
 }
