7bf70692040d3b6e13746f212110baafc950480c
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-06-03 18:21:58 +0530

    Update public-subnet.tf

--- public-subnet.tf
+++ public-subnet.tf
@@ -1,23 +1,38 @@
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.0.0/24"
-  availability_zone = "us-west-2a"
+  vpc_id            = data.aws_vpc.main_vpc.id
+  cidr_block        = "10.60.80.0/20"
+  availability_zone = "us-gov-west-1a"
   tags = {
-    Name = "public_subnet_1"
-    Subnet-Type = "public"
+    Name = "spaces-prod-public-1a"    
+    Environment  = var.env
+    Email   = var.Email
+    Project_Name = var.Project_Name
     Date = local.current_date
-    Env  = var.env
   }
 }
 
 resource "aws_subnet" "public_subnet_2" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2b"
+  vpc_id            = data.aws_vpc.main_vpc.id
+  cidr_block        = "10.60.96.0/20"
+  availability_zone = "us-gov-west-1b"
   tags = {
-    Name = "public_subnet_2"
-    Subnet-Type = "public"
+    Name = "spaces-prod-public-1b"
+    Environment  = var.env
+    Email   = var.Email
+    Project_Name = var.Project_Name
     Date = local.current_date
-    Env  = var.env
+  }
+}
+
+resource "aws_subnet" "public_subnet_3" {
+  vpc_id            = data.aws_vpc.main_vpc.id
+  cidr_block        = "10.60.112.0/20"
+  availability_zone = "us-gov-west-1c"
+  tags = {
+    Name = "spaces-prod-public-1c"
+    Environment  = var.env
+    Email   = var.Email
+    Project_Name = var.Project_Name
+    Date = local.current_date
   }
 }
